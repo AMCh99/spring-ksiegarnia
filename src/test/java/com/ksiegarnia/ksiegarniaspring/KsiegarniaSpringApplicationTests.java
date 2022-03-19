b296feade2c75859925fe7fd601b09d05ab7d544
AMCh99 <alek123252@gmail.com>
2022-03-19 01:39:14 +0100

    Zaczynamy projekt, w końcu, matko jedyna, jest to skonfigurowane i działa, chyab to też umiem to speedrun będzie, zaczynam od entities

---
+++ src/test/java/com/ksiegarnia/ksiegarniaspring/KsiegarniaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksiegarnia.ksiegarniaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KsiegarniaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
