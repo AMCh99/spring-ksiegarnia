07e24831699c68edcc04a781572447076de96ba4
AMCh99 <alek123252@gmail.com>
2022-03-20 15:51:06 +0100

    pom.xml zaktualizowany o springfox, oraz dodane dwa beany w apce.

--- src/main/java/com/ksiegarnia/ksiegarniaspring/KsiegarniaSpringApplication.java
+++ src/main/java/com/ksiegarnia/ksiegarniaspring/KsiegarniaSpringApplication.java
@@ -1,9 +1,19 @@
 package com.ksiegarnia.ksiegarniaspring;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.joda.JodaModule;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class KsiegarniaSpringApplication implements CommandLineRunner {
@@ -16,6 +26,24 @@ public class KsiegarniaSpringApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		System.out.println("siema, działam");
 
+	}
+
+	@Bean
+	public Docket productApi() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select().apis(RequestHandlerSelectors.basePackage("com.pracownia.spring.controllers"))
+				.build();
+	}
+
+	@Bean
+	public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+		MappingJackson2HttpMessageConverter jsonConverter = new MappingJackson2HttpMessageConverter();
+		ObjectMapper objectMapper = new ObjectMapper();
+		objectMapper.registerModule(new JodaModule());
+		objectMapper.enable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+		objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+		jsonConverter.setObjectMapper(objectMapper);
+		return jsonConverter;
 	}
 
 }
